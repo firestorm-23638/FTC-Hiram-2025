c6c0702a48881cf29f9b7509e7a0d48172051722
at19809 <156552513+at19809@users.noreply.github.com>
2025-09-28 17:17:37 -0400

    Made Intake Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Intake {
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake extends SubsystemBase {
+    private DcMotor intakeRoller;
+    private DigitalChannel beamBreak;
+
+    public Intake(HardwareMap hMap){
+        intakeRoller = hMap.get(DcMotor.class, "intakeRoller");
+        beamBreak = hMap.get(DigitalChannel.class, "beamBreak");
+    }
+    public CommandBase stop(){
+        return new InstantCommand(()->intakeRoller.setPower(0));
+    }
+
+    public CommandBase intakeBall(){
+        return new RunCommand(()->intakeRoller.setPower(1));
+    }
+
+    public CommandBase ejectBall(){
+        return new RunCommand(()->intakeRoller.setPower(-1));
+    }
+
+    public boolean isBeamBroken(){
+        return beamBreak.getState();
+    }
+
 }
